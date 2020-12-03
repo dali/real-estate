d3dbf6bc7560e5804fd6d0e05441e3a01bcc7dd0
benrhouma84@gmail.com <benrhouma84@gmail.com>
2020-12-03 16:47:28 +0100

    update properties pages

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,7 +1,11 @@
 module PropertiesHelper
 
-    def thumbnail property
+    def property_thumbnail (property)
         img = property.photo.present? ? property.photo.thumb.url : "placeholder.jpg"
         image_tag img, class: "property-thumb"
+    end
+
+    def property_photo_url (property)
+        property.photo.present? ? property.photo.url : asset_url("placeholder.jpg") 
     end
 end
